535562de6f18b45522cd7ae3b28b7fa3e154dfeb
xiaofeng0411 <824316775@qq.com>
2020-04-05 20:33:52 +0800

    add dog class

---
+++ dog.py
@@ -0,0 +1,14 @@
+'''
+@Author: huxiaofeng
+@Date  : 2020-04-05 20:16
+'''
+
+
+class Dog:
+    def __init__(self,name):
+        self.name = name
+
+    def bulk(self):
+        print('%s wangwagnwang' % self.name)
+
+
